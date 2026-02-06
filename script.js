a1e22da9c294f0d88ee0956d917cd155d6da6ea3
mike.hyland <mikephyland1@outlook.ie>
2026-02-06 18:53:36 +0000

    testing location

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const geoPos = document.getElementById("location");
-const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", getLocation);
 
@@ -17,23 +16,4 @@ function showLoc(pos){
     console.log("Location found!");
     geoPos.innerHTML=
         console.log("Latitude: " + pos.coords.latitude + "Longitude: " + pos.coords.longitude);
-}
-
-function showError(error) {
-    console.error("Error occurred:", error); // Debug Step 3
-    
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            geoPos.innerHTML = "User denied the request for Geolocation.";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            geoPos.innerHTML = "Location information is unavailable.";
-            break;
-        case error.TIMEOUT:
-            geoPos.innerHTML = "The request to get user location timed out.";
-            break;
-        case error.UNKNOWN_ERROR:
-            geoPos.innerHTML = "An unknown error occurred.";
-            break;
-    }
 }
